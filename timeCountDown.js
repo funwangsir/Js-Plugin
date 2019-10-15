64a42f72eb791093c45c49f2f4b78980eb7f306b
funwangsir <wangs5957@foxmail.com>
2019-10-15 18:21:03 +0800

    Revert "Add files via upload"
    This reverts commit 751e734203dd55ce60e686be5429d01f0a40de64.

--- timeCountDown.js
+++
@@ -1,9 +0,0 @@
-function timeCountDown(elementId,seconds){
-				var a = seconds;
-				function timedCount() {
-				    document.getElementById(elementId).innerHTML = a;
-				    a = (a == 0 ? 0 : a-1);
-				    setTimeout(function(){ timedCount() }, 1000);
-				}
-				timedCount();
-			}
